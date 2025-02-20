420dc4330e78cb1a0fd21dee0d61abae5fa97cbd
Jeevan <shresthaj1986@gmail.com>
2025-02-20 14:02:50 +0545

    Added Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="bg-gray-100 container">
+      <h1 className="text-xl font-semibold py-4">My Blogs</h1>
+    </div>
+  );
+};
+
+export default Header;
