da3f219d800e44fd5e9ebc9ca4e92de41e1f5086
Jeevan <shresthaj1986@gmail.com>
2025-02-20 18:11:25 +0545

    Changed font family

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import "../styles/globals.css";
+import { Open_Sans } from "next/font/google";
 import type { AppProps } from "next/app";
 
+const open_sans = Open_Sans({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={open_sans.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
 
 export default MyApp;
